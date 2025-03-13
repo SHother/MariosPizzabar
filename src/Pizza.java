eb5a8321f35ec44ff04c39fc3297d8cf37164080
Soren Hother <sora0002@stud.kea.dk>
2025-03-14 00:13:43 +0100

    test

--- src/Pizza.java
+++ src/Pizza.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.List;
 
+// Class that handles pizzas
 public class Pizza {
+    private int id;
+    private String name;
+    private double price;
+    private List<String> toppings;
+
+    public Pizza(int id, String name, double price, List<String> toppings) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+        this.toppings = toppings;
+    }
 }

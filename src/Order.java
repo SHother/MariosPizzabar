1b605dbb50b8c9060d9dd1a10d132b1c90907622
Keamanden <than0002@stud.kea.dk>
2025-03-14 15:30:01 +0100

    rename order

--- src/Order.java
+++ src/Order.java
@@ -3,16 +3,16 @@ import java.util.ArrayList;
 
 //Class that handles the specific orders
 public class Order {
-    private String navnPaaBestilling;
-    private ArrayList<Pizza> pizzasBestilt;
+    private String customerName;
+    private ArrayList<Pizza> pizzasOrdered;
     private Boolean inHouse; // Er bestilling lavet personligt eller over telefon
     private double price;
-    private LocalDateTime tidBestilt;
+    private LocalDateTime collectionTime;
 
-    public Order(String navnPaaBestilling, ArrayList<Pizza> pizzasBestilt, Boolean inHouse, double price) {
-        this.navnPaaBestilling = navnPaaBestilling;
-        this.tidBestilt = LocalDateTime.now();
-        this.pizzasBestilt = pizzasBestilt;
+    public Order(String customerName, ArrayList<Pizza> pizzasOrdered, Boolean inHouse, double price) {
+        this.customerName = customerName;
+        this.collectionTime = LocalDateTime.now();
+        this.pizzasOrdered = pizzasOrdered;
         this.inHouse = inHouse;
         this.price = price;
     }
